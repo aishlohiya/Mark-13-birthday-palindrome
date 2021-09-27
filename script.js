447853a7988a1d8e7fbd44257b5ddd6068b0152e
aishlohiya <aishlohiya@github.com>
2021-09-27 14:06:07 +0530

    ex06: Next palindrome date

--- script.js
+++ script.js
@@ -58,13 +58,80 @@ function checkAllPalindromes(date) {
     } return checkValue;
 }
 
+function leapYear(year) {
+    if (year%400 === 0)
+    return true;
 
-var realValue = {
-    day: 29,
-    month: 11,
-    year: 1992
+    if (year%100 === 0)
+    return false;
+
+    if (year%4 === 0)
+    return true;
+
+    return false;
 }
 
-var dateString = convertNumber(realValue);
+function getNextDate(date) {
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
 
-console.log(checkAllPalindromes(dateString));
+    var daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if (month===2) {
+        if(leapYear(year)) {
+            if (day>29) {
+                day = 1;
+                month = 3;
+            }
+        } else {
+            if(day>28) {
+                day = 1;
+                month = 3;
+            }
+        }
+    }
+    else {
+        if(day> daysInMonth[month-1]) {
+            day = 1;
+            month++;
+        }
+    }   
+    if (month>12) {
+        month = 1;
+        year++;
+    } 
+    return {
+        day: day,
+        month: month,
+        year: year
+    }
+}
+
+function palindromeDateCheck(date) {
+    var nextDate = getNextDate(date);
+    var counter = 0;
+
+    while (1) {
+        counter++;
+        var dateString = convertNumber(nextDate);
+        var resultValue = checkAllPalindromes(dateString);
+
+        for (var i=0; i<resultValue.length; i++) {
+            if (resultValue[i]) {
+                return [counter, nextDate];
+            }
+        }
+        nextDate = getNextDate(nextDate);
+    }
+}
+
+
+var realValue = {
+    day: 5,
+    month: 1,
+    year: 2020
+}
+
+
+console.log(palindromeDateCheck(realValue));
